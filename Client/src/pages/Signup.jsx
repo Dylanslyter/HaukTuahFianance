3cb74bcc6b2ce50c44082119387df0211343d0a5
dylan <dylan.slyter@yahoo.com>
2024-07-10 21:13:34 -0400

    messing with the signup.jsx

--- Client/src/pages/Signup.jsx
+++ Client/src/pages/Signup.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading, FormControl, FormLabel, Input, Button } from '@chakra-ui/react';
 
 const Signup = () => {
   return (
-    <Box p="8">
-      <Heading>Sign Up</Heading>
-      {/* Add your signup form here */}
+    <Box p="8" maxW="md" mx="auto" borderWidth="1px" borderRadius="lg" boxShadow="lg">
+      <Heading mb="6" textAlign="center">Sign Up</Heading>
+      <form>
+        <FormControl id="username" mb="4">
+          <FormLabel>Username</FormLabel>
+          <Input type="text" placeholder="Enter your username" />
+        </FormControl>
+        <FormControl id="email" mb="4">
+          <FormLabel>Email address</FormLabel>
+          <Input type="email" placeholder="Enter your email" />
+        </FormControl>
+        <FormControl id="password" mb="4">
+          <FormLabel>Password</FormLabel>
+          <Input type="password" placeholder="Enter your password" />
+        </FormControl>
+        <Button colorScheme="teal" type="submit" width="full" mt="4">Sign Up</Button>
+      </form>
     </Box>
   );
 };
 
 export default Signup;
+
