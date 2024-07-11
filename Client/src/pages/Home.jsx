ddd469d2988779953ecd6ed57945b5164051116c
dylan <dylan.slyter@yahoo.com>
2024-07-11 19:01:56 -0400

    messing with the home page

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import { Box, Heading, Text, Button } from '@chakra-ui/react';
+import { Box, Heading, Text, Button, VStack } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <Box p="8" maxW="md" mx="auto" borderWidth="1px" borderRadius="lg" boxShadow="lg" textAlign="center">
       <Heading mb="6">Welcome to Net Worth Tracker</Heading>
       <Text mb="4">Track your net worth easily and efficiently.</Text>
-      <Button colorScheme="teal" size="lg" mt="4">
-        Get Started
-      </Button>
+      <VStack spacing={4} mt={4}>
+        <Button as={Link} to="/signup" colorScheme="teal" size="lg" width="full">
+          Sign Up
+        </Button>
+        <Button as={Link} to="/login" colorScheme="teal" size="lg" width="full">
+          Login
+        </Button>
+      </VStack>
     </Box>
   );
 };
 
 export default Home;
+
 
