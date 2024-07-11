9b0aab0f772ef50c8f866d3c6e9aebfed0824343
dylan <dylan.slyter@yahoo.com>
2024-07-11 19:46:23 -0400

    fixing

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,9 +3,21 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: [
+      'react',
+      'react-dom',
+      'react/jsx-runtime',
+      'react/jsx-dev-runtime',
+      '@emotion/react',
+      '@emotion/styled',
+      '@chakra-ui/react'
+    ]
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:5000'
     }
   }
 });
+
