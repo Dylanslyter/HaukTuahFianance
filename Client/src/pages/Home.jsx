b1f0a67f14b9d8675b04ab8aa0cc3c435b8f7cbe
dylan <dylan.slyter@yahoo.com>
2024-07-10 21:17:08 -0400

    home.jsx

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Box, Heading, Text } from '@chakra-ui/react';
+import { Box, Heading, Text, Button } from '@chakra-ui/react';
 
 const Home = () => {
   return (
-    <Box p="8">
-      <Heading>Welcome to Net Worth Tracker</Heading>
-      <Text mt="4">Track your net worth easily and efficiently.</Text>
+    <Box p="8" maxW="md" mx="auto" borderWidth="1px" borderRadius="lg" boxShadow="lg" textAlign="center">
+      <Heading mb="6">Welcome to Net Worth Tracker</Heading>
+      <Text mb="4">Track your net worth easily and efficiently.</Text>
+      <Button colorScheme="teal" size="lg" mt="4">
+        Get Started
+      </Button>
     </Box>
   );
 };
 
 export default Home;
+
