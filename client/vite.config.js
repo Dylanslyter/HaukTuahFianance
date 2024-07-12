a9cea6aae390d966207e3002e3a70cedefa9d0ad
dylan <dylan.slyter@yahoo.com>
2024-07-11 21:46:32 -0400

    figuring this conflict out

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,21 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: [
-      'react',
-      'react-dom',
-      'react/jsx-runtime',
-      'react/jsx-dev-runtime',
-      '@emotion/react',
-      '@emotion/styled',
-      '@chakra-ui/react'
-    ]
+  root: '.', // Ensure this points to the correct directory
+  publicDir: 'public', // Public directory for static assets
+  build: {
+    outDir: 'build', // Output directory for build files
   },
   server: {
     proxy: {
-      '/api': 'http://localhost:5000'
+      '/api': 'http://localhost:3001'
     }
   }
 });
+
 
