333d8b18da04ac15cd96ede543388e675247f3ff
dylan <dylan.slyter@yahoo.com>
2024-07-11 20:18:34 -0400

    adding a liabilities page

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { Flex, Heading, Spacer, Button } from '@chakra-ui/react';
+import { Box, Flex, Heading, Spacer, Button } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <Flex as="nav" p="4" bg="teal.500" color="white" align="center">
-      <Heading as="h1" size="lg">Net Worth Tracker</Heading>
-      <Spacer />
-      <Button as={Link} to="/" colorScheme="teal" variant="outline" mr="4">Home</Button>
-      <Button as={Link} to="/login" colorScheme="teal" variant="outline" mr="4">Login</Button>
-      <Button as={Link} to="/signup" colorScheme="teal" variant="outline" mr="4">Sign Up</Button>
-      <Button as={Link} to="/assets" colorScheme="teal" variant="outline">Assets</Button>
-    </Flex>
+    <Box as="nav" p="4" bg="teal.500" color="white">
+      <Flex align="center">
+        <Heading as="h1" size="lg">Net Worth Tracker</Heading>
+        <Spacer />
+        <Button as={Link} to="/" colorScheme="teal" variant="outline" mr="4">Home</Button>
+        <Button as={Link} to="/login" colorScheme="teal" variant="outline" mr="4">Login</Button>
+        <Button as={Link} to="/signup" colorScheme="teal" variant="outline" mr="4">Sign Up</Button>
+        <Button as={Link} to="/assets" colorScheme="teal" variant="outline" mr="4">Assets</Button>
+        <Button as={Link} to="/liabilities" colorScheme="teal" variant="outline" mr="4">Liabilities</Button>
+        <Button as={Link} to="/donate" colorScheme="teal" variant="outline">Donate</Button>
+      </Flex>
+    </Box>
   );
 };
 
 export default Navbar;
+
