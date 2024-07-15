5c8eb34f0520f8af35ab89ca49945704033d184b
dylan <dylan.slyter@yahoo.com>
2024-07-15 18:27:32 -0400

    fixing this conflict

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,25 +3,21 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: [
-      'react',
-      'react-dom',
-      'react/jsx-runtime',
-      'react/jsx-dev-runtime',
-      '@emotion/react',
-      '@emotion/styled',
-      '@chakra-ui/react'
-    ]
+  publicDir: 'public',
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      external: ['/src/main.jsx'],
+    },
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-      },
+      '/api': 'http://localhost:3001'
     }
   }
 });
+
+
+
+
 
