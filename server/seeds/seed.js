67b6bbcefae8275a0a0cb96b27310ecc3b1ffca8
finntendoverse <finnlambouris616@gmail.com>
2024-07-17 20:49:09 -0400

    updated seeding script

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,7 +1,8 @@
 import { db } from "../config/connection.js";
-import { User } from "../models/index.js";
+import { User, Asset, Liability } from "../models/index.js";
 
 db.once("open", async () => {
+  // Users
   await User.deleteMany();
 
   const user1 = new User({
@@ -18,7 +19,43 @@ db.once("open", async () => {
   });
   await user2.save();
 
-  console.log(User);
+  // Assets
+  const savedUser1 = await user1.save();
+  const savedUser2 = await user2.save();
+
+  await Asset.deleteMany({});
+
+  const asset1 = new Asset({
+    name: 'Car',
+    value: 10000,
+    userId: savedUser1._id
+  });
+  await asset1.save();
+
+  const asset2 = new Asset({
+    name: 'Savings',
+    value: 5000,
+    userId: savedUser2._id
+  })
+  await asset2.save();
+
+  // Liabilities
+  await Liability.deleteMany({});
+
+  const liability1 = new Asset({
+    name: 'Car Loan',
+    value: 5000,
+    userId: savedUser1._id
+  });
+  await liability1.save();
+
+  const liability2 = new Asset({
+    name: 'Student Loan',
+    value: 10000,
+    userId: savedUser2._id
+  })
+  await liability2.save();
+
   console.log("database seeded!");
   process.exit(0);
 });
