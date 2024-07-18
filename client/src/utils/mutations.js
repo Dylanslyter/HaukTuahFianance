7340dd490dc17844ce9db53783e4c239afca62b5
dylan <dylan.slyter@yahoo.com>
2024-07-18 19:40:12 -0400

    typedefs

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,11 +3,15 @@ import { gql } from '@apollo/client';
 export const LOGIN_MUTATION = gql `
   mutation Login($email: String!, $password: String!) {
     login(email: $email,  password: $password) {
-      email
-      password
+      token
+      user {
+        _id
+        email
+        username
+      }
     }
-  }`
-;
+  }
+`;
 
 export const ADD_USER_MUTATION = gql`
   mutation AddUser($username: String!, $email: String!, $password: String!) {
